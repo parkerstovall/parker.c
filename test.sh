c8e7912c66b38081452d718a53233572c5b04260
parkerstovall <parker.stovall@gmail.com>
2026-08-19 17:21:24 -0500

    commit file rename

--- test.sh
+++
@@ -1,9 +0,0 @@
-# make -f makefiles/RunTests.mk | grep ":FAIL:" && echo "Tests Failed" || echo "All Tests Passed"
-
-make -f makefiles/RunTests.mk | grep ":FAIL:";
-
-if [[ $? -eq 1 ]]; then
-    echo "Tests Passed!";
-else 
-    echo "Tests Failed...";
-fi
